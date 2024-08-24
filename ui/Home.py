08a62c7f4d7c7683c4fa71a0cd2c8ccc711f36ec
Camilo Valenzuela <camilo.valenzuela@bain.com>
2024-08-24 15:10:49 -0400

    Agregado a streamlit

--- ui/Home.py
+++ ui/Home.py
@@ -9,7 +9,8 @@ from shapely.geometry import shape
 import geopandas as gpd
 
 from hackathonopenai import (
-    NationalMonumentsAssistant
+    NationalMonumentsAssistant, 
+    PrioritySitesAssistant
 )
 
 
@@ -23,6 +24,12 @@ df = gpd.read_file('data/parques_nacionales/data_parques.shp')
 df_kms = df.to_crs("EPSG:32633")
 national_park_expert = NationalMonumentsAssistant(df=df_kms,client=client)
 
+
+# Sitios prioritarios
+df_prioritarios = gpd.read_file('data/sitios_prioritarios/Sitios_Prioritarios.shp')
+df_prioritarios_kms = df_prioritarios.to_crs("EPSG:32633")
+priority_sites_expert = PrioritySitesAssistant(df=df_prioritarios_kms,client=client)
+
 def generate_report_expert(response: dict, title: str):
     with st.expander(f'**{response["emoji"]} {title}**: {response["resumen"]}'):
         st.write(response["evaluacion"])
@@ -32,6 +39,10 @@ def call_agents(gdf: gpd.GeoDataFrame):
     response_national_park = national_park_expert.evaluate_project(gdf)
     with st.spinner('Generando informe de Monumentos nacionales...'):
         generate_report_expert(response_national_park, "Monumentos nacionales")
+
+    response_priority_sites = priority_sites_expert.evaluate_project(gdf)
+    with st.spinner('Generando informe de Sitios prioritarios...'):
+        generate_report_expert(response_priority_sites, "Sitios prioritarios")
 
 st.title('Asistente de evaluación de proyectos fotovoltaicos')
 st.write('Welcome to the Home page!')
